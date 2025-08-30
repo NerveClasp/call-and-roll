88c62cd41228e3b14a55875c0317aa2f2b69a9f8
Romka Horokhov <7347253+NerveClasp@users.noreply.github.com>
2025-08-30 22:01:20 +0300

    chore: extract constants and types to lib

---
+++ src/lib/types.ts
@@ -0,0 +1,43 @@
+export type PlayerId = "player1" | "player2" | "player3" | "player4";
+
+export type Player = {
+  id: PlayerId;
+  name: string;
+  color: string;
+  icon: string;
+  position: number; // index of the cell on the board
+  money: number;
+  properties: { [propertyId: string]: boolean }; // dictionary of owned property ids
+};
+
+export type Cell = {
+  id: string;
+  label: string;
+  className: string;
+  icon: string;
+};
+
+export type SpecialCell = Cell & {
+  type: "special";
+  price?: never;
+  color?: never;
+};
+
+export type PropertyCell = (Cell & {
+  type: "property";
+  owner?: PlayerId;
+}) &
+  (
+    | { color: "brown"; price: 1 }
+    | { color: "lightblue"; price: 1 }
+    | { color: "purple"; price: 2 }
+    | { color: "orange"; price: 2 }
+    | { color: "red"; price: 3 }
+    | { color: "lightgreen"; price: 3 }
+    | { color: "darkgreen"; price: 4 }
+    | { color: "darkblue"; price: 5 }
+  );
+
+export type BoardCell = SpecialCell | PropertyCell;
+
+export type DiceValue = 1 | 2 | 3 | 4 | 5 | 6;
